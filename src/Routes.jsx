f7c443f5d1ea56fd3f4c6dc737a269e029a857ac
seyoung8239 <seyoung8239@gmail.com>
2022-08-24 16:27:02 +0900

    Feat: 뉴스 작성 페이지 추가

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import { Routes as RouteWrapper, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Inquiry from "./pages/Inquiry";
 import News from "./pages/News";
+import CreateNews from "./pages/CreateNews";
 
 function Routes() {
     return (
@@ -11,6 +12,7 @@ function Routes() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/inquiry" element={<Inquiry />} />
                 <Route path="/news" element={<News />} />
+                <Route path="/create_news" element={<CreateNews />} />
             </RouteWrapper>
         </>
     );
