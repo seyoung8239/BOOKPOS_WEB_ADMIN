79c25d9fb779a2b10b2b33a4ac09189127c03334
seyoung8239 <seyoung8239@gmail.com>
2022-08-24 11:58:52 +0900

    Style: 도입문의 확인 테이블 레이아웃 수정

--- src/pages/Inquiry.jsx
+++ src/pages/Inquiry.jsx
@@ -7,7 +7,6 @@ import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
 
-import Box from "@mui/material/Box";
 import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import Table from "@mui/material/Table";
@@ -95,75 +94,43 @@ function Row(props) {
                 <TableCell>{row.manager_email}</TableCell>
                 <TableCell>{row.manager_cell}</TableCell>
             </TableRow>
-            <TableRow>
-                <TableCell
-                    style={{ paddingBottom: 0, paddingTop: 0 }}
-                    colSpan={6}
-                >
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box sx={{ margin: 1 }}>
-                            <Typography
-                                variant="h6"
-                                gutterBottom
-                                component="div"
-                            >
-                                문의 상세정보
-                            </Typography>
-                            <Table size="small" aria-label="purchases">
-                                <TableRow>상호명: {row.company_name}</TableRow>
-                                <TableRow>
-                                    대표자명: {row.company_representative}
-                                </TableRow>
-                                <TableRow>
-                                    사업자 등록번호:{" "}
-                                    {row.company_business_number}
-                                </TableRow>
-                                <TableRow>
-                                    업태 / 종목: {row.company_business_category}
-                                </TableRow>
-                                <TableRow>
-                                    회사 사이트: {row.company_webpage}
-                                </TableRow>
-                                <TableRow>
-                                    사업자 주소: {row.company_addrerss}
-                                </TableRow>
-                                <TableRow>
-                                    소속작가 수: {row.company_writers}
-                                </TableRow>
-                                <TableRow></TableRow>
-                                <TableRow>
-                                    담당자명: {row.manager_name}
-                                </TableRow>
-                                <TableRow>
-                                    담당자 이메일: {row.manager_email}
-                                </TableRow>
-                                <TableRow>
-                                    담당자 휴대전화: {row.manager_cell}
-                                </TableRow>
-                                <TableRow>
-                                    담당자 유선: {row.manager_phone}
-                                </TableRow>
-                                <TableRow>
-                                    담당자 직급: {row.manager_hier}
-                                </TableRow>
-                                <TableRow>
-                                    세금계산서 담당자명: {row.tax_name}
-                                </TableRow>
-                                <TableRow>
-                                    세금계산서 담당자 이메일: {row.tax_email}
-                                </TableRow>
-                                <TableRow></TableRow>
-                                <TableRow>
-                                    북포스 관리자명: {row.admin_name}
-                                </TableRow>
-                                <TableRow>
-                                    등록하고 싶은 관리자 ID: {row.admin_id}
-                                </TableRow>
-                            </Table>
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
+            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <Typography variant="h6" gutterBottom component="div">
+                        문의 상세정보
+                    </Typography>
+                    <Table size="small" aria-label="purchases">
+                        <TableRow>상호명: {row.company_name}</TableRow>
+                        <TableRow>
+                            대표자명: {row.company_representative}
+                        </TableRow>
+                        <TableRow>
+                            사업자 등록번호: {row.company_business_number}
+                        </TableRow>
+                        <TableRow>
+                            업태 / 종목: {row.company_business_category}
+                        </TableRow>
+                        <TableRow>회사 사이트: {row.company_webpage}</TableRow>
+                        <TableRow>사업자 주소: {row.company_addrerss}</TableRow>
+                        <TableRow>소속작가 수: {row.company_writers}</TableRow>
+                        <TableRow></TableRow>
+                        <TableRow>담당자명: {row.manager_name}</TableRow>
+                        <TableRow>담당자 이메일: {row.manager_email}</TableRow>
+                        <TableRow>담당자 휴대전화: {row.manager_cell}</TableRow>
+                        <TableRow>담당자 유선: {row.manager_phone}</TableRow>
+                        <TableRow>담당자 직급: {row.manager_hier}</TableRow>
+                        <TableRow>세금계산서 담당자명: {row.tax_name}</TableRow>
+                        <TableRow>
+                            세금계산서 담당자 이메일: {row.tax_email}
+                        </TableRow>
+                        <TableRow></TableRow>
+                        <TableRow>북포스 관리자명: {row.admin_name}</TableRow>
+                        <TableRow>
+                            등록하고 싶은 관리자 ID: {row.admin_id}
+                        </TableRow>
+                    </Table>
+                </Collapse>
+            </TableCell>
         </>
     );
 }
