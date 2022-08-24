24d5057b617b13d9c7bf6c1c30bf44d5211f216d
seyoung8239 <seyoung8239@gmail.com>
2022-08-24 11:59:03 +0900

    News 관리 페이지 작업

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,19 @@
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+
 function News() {
-    return <>News</>;
+    return (
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Paper
+                sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    height: 700,
+                }}
+            ></Paper>
+        </Container>
+    );
 }
 
 export default News;
